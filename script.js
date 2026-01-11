73c76aa9bd5b1ee2205ca27024f1a1437adb5c2b
Fernando J. Richmond <90381862+FernandoJRichmond@users.noreply.github.com>
2026-01-11 21:31:48 +0100

    Mas cambios

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ const handler = () => {
 
 window.addEventListener("scroll", handler);
 window.addEventListener("load", handler);
+
+const heroImg = document.querySelector(".hero-img img");
+
+if(heroImg){
+  heroImg.addEventListener("mousemove", e=>{
+    const r = heroImg.getBoundingClientRect();
+    const x = (e.clientX - r.left) / r.width - .5;
+    const y = (e.clientY - r.top) / r.height - .5;
+    heroImg.style.transform = `rotateX(${y*-6}deg) rotateY(${x*6}deg) scale(1.02)`;
+  });
+
+  heroImg.addEventListener("mouseleave", ()=>{
+    heroImg.style.transform = "none";
+  });
+}
